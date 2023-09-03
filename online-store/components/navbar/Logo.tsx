5533f7277e8ae18ec0aab1a8c30d0ab2163ac84c
Sandip Nepali <sandeep47nepali@gmail.com>
2023-09-03 17:05:58 +0545

    added logo

--- online-store/components/navbar/Logo.tsx
+++ online-store/components/navbar/Logo.tsx
@@ -1,11 +1,16 @@
 "use client";
 import { useRouter } from "next/navigation";
 import { Aleo } from "next/font/google";
-const font = Aleo( { subsets: ['latin'] } )
+import Link from "next/link";
+const font = Aleo({ subsets: ["latin"] });
 const Logo = () => {
   return (
     <div>
-    <h2 className={` text-black text-2xl font-extrabold ${font.className}`}>Online Store</h2>
+      <Link href={'/'}>
+        <h2 className={` text-black text-2xl font-extrabold ${font.className}`}>
+          Online Store
+        </h2>
+      </Link>
     </div>
   );
 };
